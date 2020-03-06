99b3492403a8d97fb07a43f3093415ea8adb287b
Myra-Grace <59889116+myra-grace@users.noreply.github.com>
2020-03-05 22:46:57 -0500

    workshop19 messages

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,15 @@ import Footer from './Footer';
 import './App.css';
 
 function App(props) {
+  console.log('App props: ', props);
   return (
     <div className="wrapper">
-      <Header />
-      <ChatStream />
+      <Header
+      messages={props.conversation.messages} 
+      currentUser={props.currentUser} />
+      <ChatStream 
+      messages={props.conversation.messages} 
+      currentUser={props.currentUser}/>
       <Footer />
     </div>
   );
